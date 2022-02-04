3f14044fef79ce4a379e3bfeecde720738b0cb88
izenynn <me@izenynn.com>
2022-02-04 12:20:07 +0100

    fix: start orientation

--- src/player.c
+++ src/player.c
@@ -18,23 +18,23 @@ int	player_init(t_vars *vars)
 {
 	if (vars->map.start_orientation == 'N')
 	{
-		set_dir(&vars->p, -1, 0);
-		set_plane(&vars->p, 0.0, 0.66);
+		set_dir(&vars->p, 0, -1);
+		set_plane(&vars->p, -0.66, 0);
 	}
 	else if (vars->map.start_orientation == 'S')
 	{
-		set_dir(&vars->p, 1, 0);
-		set_plane(&vars->p, 0.0, -0.66);
+		set_dir(&vars->p, 0, 1);
+		set_plane(&vars->p, 0.66, 0.0);
 	}
 	else if (vars->map.start_orientation == 'W')
 	{
-		set_dir(&vars->p, 0, -1);
-		set_plane(&vars->p, -0.66, 0.0);
+		set_dir(&vars->p, -1, 0);
+		set_plane(&vars->p, 0.0, 0.66);
 	}
 	else if (vars->map.start_orientation == 'E')
 	{
-		set_dir(&vars->p, 0, 1);
-		set_plane(&vars->p, 0.66, 0.0);
+		set_dir(&vars->p, 1, 0);
+		set_plane(&vars->p, 0.0, -0.66);
 	}
 	vars->p.pos_x = (float)vars->map.p_pos[0] + 0.5;
 	vars->p.pos_y = (float)vars->map.p_pos[1] + 0.5;
